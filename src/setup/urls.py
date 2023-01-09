ea44baa81ad76a6786e2b68c034b7e85effb5e30
Prof. Leonardo Costa de Paula <leonardo.costa@ifg.edu.br>
2023-01-08 21:33:52 -0300

    Finalizando testes com templates e arquivos estáticos.

--- src/setup/urls.py
+++ src/setup/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from galeria.views import index
 
 urlpatterns = [
+    path('', include('galeria.urls')),
     path('admin/', admin.site.urls),
 ]
